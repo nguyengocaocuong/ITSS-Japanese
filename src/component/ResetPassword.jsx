09b2db5d86a38015d64e029d769f63a28f1b72c1
Hoang Long <71806715+someseconds1402@users.noreply.github.com>
2022-07-21 00:50:07 +0700

    update

--- src/component/ResetPassword.jsx
+++ src/component/ResetPassword.jsx
@@ -1,29 +1,64 @@
-import React from 'react'
-import '../assets/css/login-form.scss'
+import React, { useRef } from 'react'
+import '../assets/css/reset-password.scss'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export const ResetPassword = () => {
-  const navigation = useNavigate();
+    const navigation = useNavigate();
+
+    const emailRef = useRef(null);
+    const otpRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const handleSendOtp = () => {
+        axios.get(`https://app-matching-friend.herokuapp.com/accounts/reset-pass?email=${emailRef.current.value}`)
+    }
+
+    const handleResetPass = () => {
+        axios.post("https://app-matching-friend.herokuapp.com/accounts/check-update-pass", {
+            email: emailRef.current.value,
+            otp: otpRef.current.value,
+            newPass: passwordRef.current.value
+        }).then(res => {
+            // console.log(res);
+            if (res.data) {
+
+                const { personId, name, sex } = res.data;
+                localStorage.setItem('user', JSON.stringify({ userId: personId, name, sex }))
+                navigation('/login/login-form')
+            }
+
+            else{
+
+            }
+        }).catch(e => {
+            console.error(e);
+        })
+    }
+
     return (
         <div className="login-form__right">
             <div className="icon-close"><p onClick={() => navigation('/login/login-form')}>X</p></div>
             <div className="login-form__input">
                 <div className="group-input">
-                    <input type="text" placeholder='Username' />
+                    <input ref={emailRef} type="text" placeholder='Email' />
+                    <p className="recieve-otp" onClick={() => handleSendOtp()}>Recieve OTP</p>
                 </div>
                 <div className="group-input">
                     <i className='bx bx-low-vision'></i>
-                    <input type="password" placeholder='New Password' />   
+                    <input ref={otpRef} type="text" placeholder='OTP' />
                 </div>
                 <div className="group-input">
                     <i className='bx bx-low-vision'></i>
-                    <input type="password" placeholder='Retype Password' />   
+                    <input ref={passwordRef} type="password" placeholder='Password' />
                 </div>
 
-                <button className="btn" >Submit</button>
                 
+
+                <button className="btn" onClick={() => handleResetPass()} >Submit</button>
+
             </div>
-            
+
         </div>
     )
 }
